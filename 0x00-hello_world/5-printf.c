732f8ad405a8f3fb4da5c2967d197434e656e9f1
Kodjo Emmanuel Aziawor <kodjo.aziawor@gmail.com>
2022-07-03 14:17:25 -0700

    rfrf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Short description, single line
+* void: Description of parameter x
+*
+* Description: print text with
+* Return:return zero
+*/
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
