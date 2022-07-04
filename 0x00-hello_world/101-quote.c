29a2444fb257da607ad0908df26a90b70a50074c
Kodjo Emmanuel Aziawor <kodjo.aziawor@gmail.com>
2022-07-03 19:33:05 -0700

    quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Short description, single line
+* void: Description of parameter x
+*
+* Description: print text with
+* Return:return zero
+*/
+int main(void)
+{ write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 61);
+return (1);
+}
